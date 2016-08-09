7b96de15913a75d8f5e823aac6eb76edefefc575
Tyler Shimko <TShimko126@gmail.com>
2016-08-09 11:21:43 -0700

    Add valve numbering to switching

--- gui.py
+++ gui.py
@@ -5,39 +5,39 @@ from kivy.uix.gridlayout import GridLayout
 
 client = ModbusTcpClient('192.168.1.3')
 
-
-
-labels = ["PBS",
-          "Antibody",
-          "bBSA",
-          "Neutravidin",
-          "Extra 1",
-          "Extra 2",
-          "Protein",
-          "Waste",
-          "Buttons 1",
-          "Buttons 2",
-          "Sandwiches 1",
-          "Sandwiches 2",
-          "Neck",
-          "In",
-          "Out"]
+labels = {"PBS": 0,
+          "Antibody": 1,
+          "bBSA": 2,
+          "Neutravidin": 3,
+          "Extra 1": 4,
+          "Extra 2": 5,
+          "Protein": 6,
+          "Waste": 7,
+          "Buttons 1": 8,
+          "Buttons 2": 9,
+          "Sandwiches 1": 10,
+          "Sandwiches 2": 11,
+          "Neck": 1,
+          "In": 1,
+          "Out": 1}
 
 
 class PressureButton(Button):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, valve_number, *args, **kwargs):
         super(PressureButton, self).__init__(*args, **kwargs)
         self.pressure_state = True
         self.background_normal = ''
         self.background_color = (.94, .05, .05, 1.0)
+        self.valve_number = valve_number
 
 
 class Geppetto(App):
     def build(self):
         layout = GridLayout(cols=4, row_force_default=True,
                             row_default_height=70)
-        for i in labels:
-            button = PressureButton(text=i,
+        for i in labels.keys:
+            button = PressureButton(valve_number=labels[i],
+                                    text=i,
                                     background_normal='',
                                     background_color=(.94, .05, .05, 1.0))
 
@@ -50,11 +50,11 @@ class Geppetto(App):
 
 def change_pressure_state(instance):
     if instance.pressure_state:
-        pressurize(instance.valve_number())
+        pressurize(instance.valve_number)
         instance.pressure_state = False
         instance.background_color = (.05, .5, .94, 1.0)
     else:
-        depressurize(instance.valve_number())
+        depressurize(instance.valve_number)
         instance.pressure_state = True
         instance.background_color = (.94, .05, .05, 1.0)
     print(instance.pressure_state)
