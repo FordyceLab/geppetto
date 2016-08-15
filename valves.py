c826b69f375583443b1c746a021b7b9a54967384
Tyler Shimko <TShimko126@gmail.com>
2016-08-15 16:50:11 -0700

    Correct valve changing direction

--- valves.py
+++ valves.py
@@ -7,12 +7,13 @@ client = ModbusTcpClient('192.168.1.3')
 def pressurize(valve_number):
     register_number = 512 + valve_number
     state = client.read_coils(register_number, 1).bits[0]
-    if not state:
-        client.write_coil(valve_number, True)
+    if state:
+        client.write_coil(valve_number, False)
 
 
 def depressurize(valve_number):
     register_number = 512 + valve_number
     state = client.read_coils(register_number, 1).bits[0]
-    if state:
-        client.write_coil(valve_number, False)
+    state = False
+    if not state:
+        client.write_coil(valve_number, True)
