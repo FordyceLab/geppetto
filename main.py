8cc2454c163cf24a7cf3c9cf6d288137d476d474
Tyler Shimko <TShimko126@gmail.com>
2016-09-27 11:50:41 -0700

    Exit program if an error is raised (quitting the first app)

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from gui import Geppetto, ChooseConfigFile
+import sys
 
 if __name__ == "__main__":
-    ChooseConfigFile().run()
-    Geppetto().run()
+    try:
+        ChooseConfigFile().run()
+    except Exception as e:
+        sys.exit(0)
+    try:
+        Geppetto().run()
+    except Exception as e:
+        sys.exit(0)
